64823b507db82612ed399428eda639b82c1da16c
apra93 <dd.apra93@gmail.com>
2018-03-29 10:03:05 -0700

    ENH: Calibrations can now be saved and loaded

--- sxr/exceptions.py
+++ sxr/exceptions.py
@@ -2,14 +2,22 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-class SxrpyException(Exception):
-    """
-    Base exception class for the SXR-python environment.
-    """
+
+class SxrPythonException(Exception):
+    """Base exception class for the SXR-python environment."""
     pass
 
-class InputError(Exception):
-    """
-    Exception raised when invalid inputs are provided.
-    """
+
+class InputError(SxrPythonException):
+    """Exception raised when invalid inputs are provided."""
+    pass
+
+
+class Lr5816Error(SxrPythonException):
+    """Base exception class for the run 16 LR58 mcgrain experiment."""
+    pass
+
+
+class NotCalibratedError(Lr5816Error):
+    """Exception raised when there is no current calibration."""
     pass
