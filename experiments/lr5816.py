9f2bf01f4057b2f29f8ed7822403530871cc2a5b
apra93 <dd.apra93@gmail.com>
2018-03-28 21:27:49 -0700

    MAINT: Moving devices around and cleaning.

--- experiments/lr5816.py
+++ experiments/lr5816.py
@@ -2,7 +2,6 @@ import time
 import logging
 
 import numpy as np
-from ophyd.sim import SynAxis
 
 from sxr.plans import mcgrain_scan as _mcgrain_scan
 from sxr.devices import ErrorIMS, Sequencer, McgrainPalette
@@ -13,13 +12,12 @@ logger = logging.getLogger(__name__)
 class User(object):
     """User class for the LR58 Mcgrain experiment."""
     # Devices
-    mono = ErrorIMS("SXR:MON:MMS:06", name="Monochrometer")
-    sequencer = Sequencer("ECS:SYS0:2", name="sequencer")
-    palette = McgrainPalette(name="Mcgrain Palette")
-    syn_motor_1 = SynAxis(name="Syn Motor 1")
+    _mono = ErrorIMS("SXR:MON:MMS:06", name="Monochrometer")
+    _sequencer = Sequencer("ECS:SYS0:2", name="sequencer")
+    _palette = McgrainPalette(name="Mcgrain Palette")
 
     def __init__(self, *args, **kwargs):
-        self.palette.accept_calibration(
+        self._palette.accept_calibration(
             np.array([-9.54519187, -2.99960937, -2.       ]), 
             np.array([ 12.57935063,  -2.89960937,  -2.    ]), 
             np.array([ -9.24544125, -86.26064453,  -2.    ]))
@@ -27,7 +25,7 @@ class User(object):
     def mcgrain_scan(self, mono_start, mono_stop, mono_steps, palette_steps, 
                      *args, **kwargs):
         yield from _mcgrain_scan(
-            self.mono, self.palette, self.sequencer, 
+            self._mono, self._palette, self._sequencer, 
             mono_start, mono_stop, mono_steps, 
             palette_steps, 
             *args, **kwargs)
--- sxr/beamline.py
+++ sxr/beamline.py
@@ -1,5 +1,19 @@
 from hutch_python.utils import safe_load
+from ophyd.sim import SynAxis
 
+from .devices import McgrainPalette, ErrorIMS, Sequencer
 
-with safe_load('example'):
-    1/0
+# Test axes that may come in handy
+with safe_load('Virtual Motors'):
+    virtual_motor_1 = SynAxis(name='Virtual Motor 1')
+    virtual_motor_2 = SynAxis(name='Virtual Motor 2')
+    virtual_motor_3 = SynAxis(name='Virtual Motor 3')
+
+with safe_load('Mcgrain Palette'):
+    palette = McgrainPalette(name="Mcgrain Palette")
+
+with safe_load('Monochrometer'):
+    mono = ErrorIMS("SXR:MON:MMS:06", name="Monochrometer")
+
+with safe_load('Event Sequencer'):
+    sequencer = Sequencer("ECS:SYS0:2", name="Event Sequencer")
